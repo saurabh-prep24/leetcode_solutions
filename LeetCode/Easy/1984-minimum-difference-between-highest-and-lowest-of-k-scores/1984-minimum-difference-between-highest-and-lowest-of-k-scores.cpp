03c9b35bf957811d930aeb1939ade9fe2fafb3df
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-01-26 00:18:25 +0530

    1984-minimum-difference-between-highest-and-lowest-of-k-scores

--- LeetCode/Easy/1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ LeetCode/Easy/1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
+        /*
+            - Sort the arr
+            - use sliding window of k size
+            - update mini and maxi while moving forward
+            - keep min(maxi-mini) as ans
+            * Time - O(N)
+            * Space - O(1)
+        */
         sort(nums.begin(), nums.end());
         int mini = INT_MAX, maxi = INT_MIN;
         for(int i =0; i < k; i++){
