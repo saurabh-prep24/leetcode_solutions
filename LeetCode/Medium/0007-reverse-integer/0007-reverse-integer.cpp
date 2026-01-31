05961b93d0634c9c52357988320c8eb8b5558aa8
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-01-31 18:19:59 +0530

    0007-reverse-integer

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,8 +4,14 @@ public:
         int rev = 0;
         while (x != 0) {
             int d = x % 10;
+            // check for 10*rev > INT_MAX or 10*rev < INT_MIN
+            // but instead of multiply we can do divide to avoid interger
+            // overflow no need to add digit as digit < 10 so only division by
+            // 10 can handle overflow
             if (rev > (INT_MAX) / 10 || rev < (INT_MIN) / 10)
                 return 0;
+
+            // update rev
             rev = 10 * rev + d;
             x /= 10;
         }
