0e08fa77a4fccc7c936abbb40f7f1479cafacb50
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-01 02:07:29 +0530

    0008-string-to-integer-atoi

--- LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -12,9 +12,9 @@ public:
             return ans;
 
         // sign
-        bool neg = 0;
+        int sign = 1;
         if (s[i] == '-') {
-            neg = 1;
+            sign = -1;
             i++;
         } else if (s[i] == '+')
             i++;
@@ -22,12 +22,13 @@ public:
         while (i < n && s[i] >= '0' && s[i] <= '9') {
             int dig = s[i] - '0';
             if (ans > (INT_MAX - dig) / 10 ||
-                neg && ans < neg*(INT_MIN + dig) / 10) {
-                return neg ? INT_MIN : INT_MAX;
+                sign == -1 && ans < (INT_MIN + dig) / 10) {
+                return sign == -1 ? INT_MIN : INT_MAX;
             }
             ans = ans * 10 + dig;
             i++;
         }
-        return neg ? -ans : ans;
+        // cout<<sign;
+        return sign * ans;
     }
 };
