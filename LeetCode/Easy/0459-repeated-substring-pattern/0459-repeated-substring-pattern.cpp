ec1199bd1e8759708353ffbf5eb3808dcfbcef5a
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-01 18:35:11 +0530

    0459-repeated-substring-pattern

--- LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        if(s.size()%2 != 0){
-            return 0;
-        }  
-        unordered_map<char, int> mp;
-        int minCount = INT_MAX;
-        for(auto i:s){
-            mp[i]++;
-        }
-        for(auto i:mp){
-            if(i.second == 1)
-                return 0;
-            minCount = min(minCount, i.second);
-        }
-        cout<<minCount<<endl;
-        int req = 0;
-        for(auto i: mp){
-            if(i.second % minCount != 0){
-                return 0;
-            }else{
-                req += i.second / minCount;
-            }
-        }
+        // approach1 : trick using copy of s and using find s on s+s
+        // use t = s+s
+        // remove first and last char from t
+        // if s still exists into t
+        // then return 1
+        // ex - s = abab | t = abababab
+        // t' = bababa contain 'abab' (s)
+        // Time: O(n*n)
+        // time complexity of std::string::find in C++ is
+        // O(n × m) in the worst case
+        // Space: O(n)
+        // string t = s + s;
+        // t = t.substr(1, t.size() - 2);
+        // cout << t << " " << s << endl;
+        // return t.find(s) != string::npos;
 
-        int i =0, j = req;
-        while(j<s.size()){
-            if(s[i]!=s[j]){
-                return 0;
+        // approach2 : brute force
+        // get all sub possible till n/2
+        // check if remaining string only contains curr substr
+        int n = s.size();
+        for (int l = 1; l <= n / 2; l++) {
+            // if there are remaining elem with curr substr len then skip
+            if (n % l != 0)
+                continue;
+
+            // consider match
+            bool isFound = 1;
+            // base substr for comp
+            string baseSubStr = s.substr(0, l);
+            for (int i = l; i < n; i += l) {
+                // curr substr to be compared with base substr
+                string currSubStr = s.substr(i, l);
+                // if not same then break and continue with next len
+                if (baseSubStr != currSubStr) {
+                    isFound = 0;
+                    break;
+                }
             }
-            i++;
-            j++;
+            // if match found with curr len then return 1
+            if (isFound) {
+                return 1;
+            }
         }
-        return 1;
+        // return 0 as no match found
+        return 0;
     }
 };
