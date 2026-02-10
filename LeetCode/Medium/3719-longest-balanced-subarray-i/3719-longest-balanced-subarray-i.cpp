2b12f8452c1aecf9da6c0a0dadc81c6dd5e59d38
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-10 22:53:41 +0530

    3719-longest-balanced-subarray-i

--- LeetCode/Medium/3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ LeetCode/Medium/3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+    // Given N <= 1500 so brute force can work with all sub-arr
+    // Store unique odd and even into set for each sub-arr
+    // whenever sizes of sets are same then update ans
+    // Time: O(N*N)
+    // Space: O(N)
     int longestBalanced(vector<int>& nums) {
+        // ans and size
         int ans = 0, n = nums.size();
-        set<int> odd;
-        set<int> even;
-        for (int j = 0; j < n - 1; j++) {
-            for (int i = j; i < n; i++) {
-                if (nums[i] % 2 == 0) {
-                    even.insert(nums[i]);
-                } else {
-                    odd.insert(nums[i]);
-                }
-                if (even.size() == odd.size()) {
-                    // cout << j << " " << i << " " << i - j + 1 << endl;
-                    ans = max(ans, i - j + 1);
-                }
-            }
+        // keep odd and even into set for unique
+        unordered_set<int> odd;
+        unordered_set<int> even;
+        // loop via all subsets from i = 0 -> n-1
+        for (int start = 0; start < n - 1; start++) {
+            // clear both sets before each sub arr
             odd.clear();
             even.clear();
+            for (int end = start; end < n; end++) {
+                if (nums[end] % 2 == 0) {
+                    // even
+                    even.insert(nums[end]);
+                } else {
+                    // odd
+                    odd.insert(nums[end]);
+                }
+                if (even.size() == odd.size()) {
+                    // update ans when size are equal 
+                    // store max len of sub arr [end-start+1]
+                    ans = max(ans, end - start + 1);
+                }
+            }
         }
+        // final ans
         return ans;
     }
 };
