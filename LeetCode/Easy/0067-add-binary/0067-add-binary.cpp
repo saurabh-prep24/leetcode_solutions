1258fd651e9fbd96e4e8df4a199a9511012932a6
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-16 00:01:19 +0530

    0067-add-binary

--- LeetCode/Easy/0067-add-binary/0067-add-binary.cpp
+++ LeetCode/Easy/0067-add-binary/0067-add-binary.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    int add(char a, char b, int c){
-        int s = c;
-        if(a == b && a=='1'){
-            s += 2;
-        }else if(a == '1' || b == '1'){
-            s += 1;
-        }
-        return s;
-    }
+    // Time: O(N)
+    // Space: O(1)
     string addBinary(string a, string b) {
+        // store ans
         string ans = "";
-        // reverse both
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        int i =0, j=0, c=0;
-        // loop each digits
-        while(i<a.size() || j<b.size() || c>0){
-            // if both then add with carry
-            if(i<a.size() && j<b.size()){
-                int t= add(a[i], b[j], c);
-                ans += to_string(t%2);
-                c = t/2;
-                i++;
-                j++;
-            }else if(i<a.size()){
-                int t= add(a[i], '0',c);
-                c = t/2;
-                ans += to_string(t%2);
-                i++;
-            }else if(j<b.size()){
-                int t= add('0', b[j],c);
-                c = t/2;
-                ans += to_string(t%2);
-                j++;
-            }else if(c>0){
-                // add carry finally
-                ans += to_string(c);
-                break;
+        // carry
+        int c = 0;
+        // start from last to avoid reverse saves O[n]
+        int i = a.size() - 1, j = b.size() - 1;
+        // loop till any rem dig or c > 0
+        while (i >= 0 || j >= 0 || c > 0) {
+            // curr sum of dig
+            int s = c;
+            if (i >= 0) {
+                s += a[i]-'0';
+                i--;
             }
+            if (j >= 0) {
+                s += b[j]-'0';
+                j--;
+            }
+            // update ans 
+            ans = to_string(s % 2)+ans;
+            // update carry
+            c = s / 2;
         }
-        // reverse for actual ans
-        reverse(ans.begin(), ans.end());
+        // return ans
         return ans;
     }
 };
