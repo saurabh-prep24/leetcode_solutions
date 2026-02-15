8e435c4e3b44d09d6accbdd39e45712e81b89f6f
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-16 01:48:17 +0530

    0242-valid-anagram

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())  return 0;
+        unordered_map<char, int> mp;
+        for(int i =0;i<s.size();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto i:mp){
+            if(i.second != 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
