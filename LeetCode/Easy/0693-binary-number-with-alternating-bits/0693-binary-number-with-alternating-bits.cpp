f0add50747b8b6e1562bba02566b7a6fa569ab3f
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-19 00:49:30 +0530

    0693-binary-number-with-alternating-bits

--- LeetCode/Easy/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ LeetCode/Easy/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
+    bool trick(int n) {
+        /*
+        If bits are alternating:
+        n      = 101010
+        n>>1   = 010101
+        XOR    = 111111
+        So x becomes all 1s.
+        And a number that is all 1s satisfies:
+        x & (x + 1) == 0
+        Example:
+        111111
+        +    1
+        ------
+        1000000
+        ------
+
+        AND = 0
+         111111
+        1000000
+        -------
+        0000000
+        -------
+        */
+        unsigned int temp = n ^ (n >> 1);
+        return (temp & (temp + 1)) == 0;
+    }
     bool hasAlternatingBits(int n) {
+        return trick(n);
+
         // check first set bit
         bool isSetBit = n & 1;
         // loop
         while (n) {
             // check if curr bit is same as isSetBit flag
-            if ((n&1) != isSetBit)
+            if ((n & 1) != isSetBit)
                 // not matched then return false
                 return 0;
 
