733bc6941809c0f6e891de1ab569539b889b9734
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-22 02:48:23 +0530

    0191-number-of-1-bits

---
+++ LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // time: O(32)
+    int simple(int n) {
+        int s = 0;
+        while (n) {
+            n = n & 1;
+            n = n >> 1;
+            s++;
+        }
+        return s;
+    }
+
+    // time: O(setBitsCount)
+    int opt(int n) {
+        int s = 0;
+        while (n) {
+            n = n & (n - 1);
+            s++;
+        }
+        return s;
+    }
+    int hammingWeight(int n) {
+        cout << simple(n) << endl;
+        cout << opt(n) << endl;
+        return opt(n);
+    }
+};
