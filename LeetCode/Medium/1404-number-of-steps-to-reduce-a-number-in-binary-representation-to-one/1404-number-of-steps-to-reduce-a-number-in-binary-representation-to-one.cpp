04385fa2c879310c847a3bcde8e1f4ca11ea7435
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-02-27 01:57:26 +0530

    1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one

---
+++ LeetCode/Medium/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int convert(string s) {
+        unsigned long long a = 0;
+        int i = s.size() - 1;
+        unsigned long long int f = 1; // 2 places
+        while (i >= 0) {
+            a = a + f * (s[i] - '0');
+            i--;
+            f *= 2;
+        }
+        int ans = 0;
+        while (a != 1) {
+            if (a & 1) {
+                a += 1;
+            } else {
+                a = a / 2;
+            }
+            ans++;
+        }
+
+        return ans;
+    }
+
+    int processEachBit(string s) {
+        // consider
+        // 100 -> 4 => ans = +1(/2) +1(/2) -> 2 [reached 1]
+        // whenever there is last bit not set
+        // then only 1 operation /2 -> right shift
+
+        // 101 -> 5 => ans = +1(odd + 1)
+        // 110 -> 6 => ans = +1(/2) [right shift]
+        // 011 -> 3 => ans = +1(add 1)
+        // 100 -> 4 => ans = +1(/2)
+        //  10 -> 2 => ans = +1(/2)
+        //   1 -> 1 => ans = +1(/2) [reached 1]
+        // so whenever there last bit is set then we are carrying 1 due to sum
+        // and then divide by 2 as it got even -> which right shifts
+
+        // MSB[most sig bit] can be skipped at last to get 1
+
+        int ans = 0;
+        int carry = 0;
+        for (int i = s.size() - 1; i > 0; i--) {
+            int currBit = s[i] - '0' + carry;
+            if (currBit == 1) {
+                // odd
+                ans += 2;
+                carry = 1;
+            } else {
+                ans += 1;
+            }
+        }
+        // process last carry also so divide by 2 [since last dig rem = 1]
+        // 1+carry = 1 + 1 = 2 [10] => div by 2 => 1
+        if(carry){
+            ans++;
+        }
+        return ans;
+    }
+
+    int numSteps(string s) {
+        // runtime err as overflow since len is <=500 even
+        // long long cant store it
+        // return convert(s);
+
+        return processEachBit(s);
+    }
+};
