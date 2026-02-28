80e37bb0fa8e246996aa34c4770731a4a80eadd0
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-01 01:35:56 +0530

    1680-concatenation-of-consecutive-binary-numbers

---
+++ LeetCode/Medium/1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    /*
+        1 = [1]
+        2 = [10]
+        3 = [11]
+        4 = [100]
+        5 = [101]
+        6 = [110]
+        7 = [111]
+        8 = [1000]
+
+        From above we can observe that on every power of 2
+        we are using 1 more bits
+        For concatenations->
+        left shift to no. of ongoing bits then add curr no
+        Do module while adding up to avoid overflow
+        for power of 2 -> n&(n-1)==0 or
+        use factor which get 2x when i == factor
+        ex. start with f = 2, bits = 1, ans = 0
+        i = 1
+            => ans = (0<<1) + i = 00 + 1 = 1
+        i = 2 (bits++ | f = 4)
+            => ans = (ans<<2) + i = 100 + 10 = 110
+        i = 3
+            => ans = (ans<<2) + i = 11000 + 11 = 11011
+        i = 4 (bits++ | f=4)
+            => ans = (ans<<4) + i = 11011000 + 100 = 11011100
+    */
+    int concatenatedBinary(int n) {
+        long long ans = 0;
+        int factor = 2;
+        int bitsCount = 1;
+        int mod = 1e9 + 7;
+        for (int i = 1; i <= n; i++) {
+            if (i == factor) { // or [i&(i-1)==0]
+                bitsCount++;
+                factor *= 2;
+            }
+            ans = ((ans << bitsCount) % mod + i) % mod; // concatenate with i
+        }
+        return (int)ans;
+    }
+};
