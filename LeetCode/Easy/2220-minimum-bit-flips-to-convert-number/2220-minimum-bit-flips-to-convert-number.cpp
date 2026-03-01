9dbd01f1e6945260b6193c870232ec5b7e121e39
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-02 02:31:40 +0530

    2220-minimum-bit-flips-to-convert-number

--- LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         int flips = 0;
+        // loop till any set bit remaining
         while (start || goal) {
+            // check if both bits at end are equal
+            // if not then increament flips
             if ((start & 1) != (goal & 1)) {
                 flips++;
             }
+            // right shift both
             start >>= 1;
             goal >>= 1;
         }
+        // return ans
         return flips;
     }
 };
