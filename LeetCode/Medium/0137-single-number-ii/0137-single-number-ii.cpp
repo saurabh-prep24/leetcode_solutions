3c10adde9a9c7bd0e296d640be21292576bcc707
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-04 21:49:08 +0530

    0137-single-number-ii

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    // time: O(32n)
+    // space: O(32)
+    int bitOpt1(vector<int>& nums) {
         int n = nums.size();
         // count of set bits
         vector<int> count(32, 0);
@@ -23,5 +25,30 @@ public:
             }
         }
         return ans;
+    }
+
+    // time: O(32n)
+    // space: O(32)
+    int bitsOpt2(vector<int>& nums) {
+        int ans = 0;
+        // count set bits for each nums per bit
+        for (int i = 0; i < 32; i++) {
+            // total set bits for ith bit
+            int sumSetBits = 0;
+            // add set bits to sum
+            for (auto n : nums) {
+                sumSetBits += (n >> i) & 1;
+            }
+            // build ans
+            if (sumSetBits % 3 != 0) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+
+    int singleNumber(vector<int>& nums) {
+        // return bitsOpt1(nums);
+        return bitsOpt2(nums);
     }
 };
