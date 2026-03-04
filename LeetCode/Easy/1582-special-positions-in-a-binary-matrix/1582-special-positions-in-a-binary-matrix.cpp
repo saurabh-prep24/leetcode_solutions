980550be45bb454e90d380fd397d8fd45fdf2e51
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-04 20:21:55 +0530

    1582-special-positions-in-a-binary-matrix

--- LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -24,7 +24,7 @@ public:
         }
         return mat[r][c] == 1;
     }
-    int numSpecial(vector<vector<int>>& mat) {
+    int brute(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
         int ans = 0;
@@ -36,5 +36,39 @@ public:
             }
         }
         return ans;
+    }
+
+    int opt(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        vector<int> rowCount1(n, 0);
+        vector<int> colCount1(m, 0);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    rowCount1[i]++;
+                }
+                if (mat[i][j] == 1) {
+                    colCount1[j]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    if (rowCount1[i] == 1 && colCount1[j] == 1) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    int numSpecial(vector<vector<int>>& mat) {
+        // return brute(mat);
+
+        return opt(mat);
     }
 };
