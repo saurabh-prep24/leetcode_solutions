26ae5b267c08b6f782bce141b1da64b8b9dea63f
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-08 21:39:28 +0530

    03-08-2026 1980-find-unique-binary-string

--- LeetCode/Medium/1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ LeetCode/Medium/1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -3,11 +3,15 @@ public:
     // time: O(n * 2^n)
     // space: On)
     string bitMasking(vector<string>& nums) {
+        // store unique
         unordered_set<string> unique;
         int n = nums.size();
+        // add input str
         for (auto i : nums) {
             unique.insert(i);
         }
+        // convert to required format with bitmask 
+        // this is used to create all sub-seq
         for (int mask = 0; mask < (1 << n); mask++) {
             string s(n, '0');
             for (int i = 0; i < n; i++) {
@@ -19,9 +23,6 @@ public:
                 return s;
             }
         }
-        // for(auto i:unique){
-        //     cout<<i<<endl;
-        // }
         return "";
     }
 
@@ -29,15 +30,26 @@ public:
     // space: O(n)
     void solveRec(vector<string>& nums, unordered_set<string>& unique,
                   string temp, string& ans) {
+        // temp reached nums.size() 
+        // [same as str len in ques otherwise len str should be used]
         if (temp.size() == nums.size()) {
+            // check if not present in set
             if (unique.count(temp) == 0) {
+                // update ans
                 ans = temp;
             }
+            // return
             return;
         }
-
+        // call with temp + '0'
         solveRec(nums, unique, temp + '0', ans);
+        // any str works so return ans if already updated
+        if (ans != "") {
+            return;
+        }
+        // call for temp + '1'
         solveRec(nums, unique, temp + '1', ans);
+        return;
     }
     string solveWithDFS(vector<string>& nums) {
         unordered_set<string> unique;
@@ -50,7 +62,6 @@ public:
         return ans;
     }
 
-    
     string findDifferentBinaryString(vector<string>& nums) {
         // return bitMasking(nums);
         return solveWithDFS(nums);
