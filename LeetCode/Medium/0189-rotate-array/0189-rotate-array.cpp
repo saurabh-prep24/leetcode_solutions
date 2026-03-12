a15a8d059cb4af29649ecb9aff8afff25e7529fd
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-13 00:25:07 +0530

    03-13-2026 0189-rotate-array

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
@@ -26,26 +26,43 @@ public:
         vector<int> rem;
 
         // copy last n-k elem to rem so later we can copy to front
-        for(int i = n-k;i<n;i++){
+        for (int i = n - k; i < n; i++) {
             rem.push_back(nums[i]);
         }
 
         // move starting k elem to forward by k places
-        for(int i = n-1; i-k>=0 ; i--){
-            nums[i] = nums[i-k];
+        for (int i = n - 1; i - k >= 0; i--) {
+            nums[i] = nums[i - k];
         }
 
         // move last n-k elem to front
-        for(int i = 0;i<rem.size();i++){
+        for (int i = 0; i < rem.size(); i++) {
             nums[i] = rem[i];
         }
     }
 
+    // time: O(n)
+    // space: O(1)
+    void arrInverse(vector<int>& nums, int k) {
+        // [1 2 3 4 5]
+        // k = 3
+        // [3 4 5 1 2]
+        // a = [1 2] | b = [3 4 5]
+        // ab - given | ba - needed
+        // reverse full arr
+        // reverse [0, k] then [k+1, n]
+        // [5 4 3 2 1]
+        // [3 4 5][1 2] - ans
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
 
         // rotateUptoK(nums, k);
-        copyToRotate(nums, k);
+        // copyToRotate(nums, k);
+        arrInverse(nums, k);
     }
 };
