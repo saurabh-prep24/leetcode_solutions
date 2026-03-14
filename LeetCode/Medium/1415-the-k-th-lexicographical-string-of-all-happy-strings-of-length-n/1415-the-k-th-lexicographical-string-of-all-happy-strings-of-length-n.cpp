fa8a377344ff1429da1056fa7790b9887b6f8a3f
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-15 02:07:41 +0530

    03-15-2026 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n

--- LeetCode/Medium/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ LeetCode/Medium/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -4,46 +4,59 @@ public:
     char allowed[3] = {'a', 'b', 'c'};
 
     // build all happy str
-    void getAllHappyStr(int n, string curr, vector<string>& arr, char last,
-                        int& k) {
+    // void getAllHappyStr(int n, string curr, vector<string> &arr, char last,
+    // int& k) {
+    void getAllHappyStr(int n, string curr, int& k, string &ans) {
         if (curr.size() == n) {
             // push to list of happy arr
-            arr.push_back(curr);
+            // arr.push_back(curr);
+
+            // cout << curr << endl;
             k--;
+            if (k == 0)
+                ans = curr;
             return;
         }
 
         // loop for each allowed chars
         for (auto i : allowed) {
-            if (k == 0) {
-                return;
-            }
             // ensure last != i -> curr[i+1] != curr[i](last)
-            if (i != last) {
-                getAllHappyStr(n, curr + i, arr, i, k);
+            if (curr.size() > 0 && i == curr.back()) {
+                continue;
+            }
+            // getAllHappyStr(n, curr + i, arr, i, k);
+            getAllHappyStr(n, curr + i, k, ans);
+            if (ans.size() > 0) {
+                return;
             }
         }
     }
+
     string getHappyString(int n, int k) {
         vector<string> happyStrs;
         string curr = "";
         int reqIndex = k;
         // populate happy str
-        getAllHappyStr(n, curr, happyStrs, ' ', k);
-        // sort all strs -> not needed
+        // getAllHappyStr(n, curr, happyStrs, ' ', k);
+        // OPT:
+        // 1. sort all strs -> not needed
         // as its being created in sorted order only
-        // instead pass k and return once kth index is populated
-        // for(auto i: happyStrs){
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
         // sort(happyStrs.begin(), happyStrs.end());
+
+        // 2. instead pass k and return once kth index is populated
+        // also we dont need to store all happy str just use ans
+        // return when k th str is found
+        string ans = "";
+        getAllHappyStr(n, curr, k, ans);
+
         // for(auto i: happyStrs){
         //     cout<<i<<" ";
         // }
         // cout<<endl;
         // if k>len then "" else return (k-1)th happy str
         // return k > happyStrs.size() ? "" : happyStrs[k - 1];
-        return reqIndex > happyStrs.size() ? "" : happyStrs[happyStrs.size() - 1];
+        // return reqIndex > happyStrs.size() ? "" : happyStrs[happyStrs.size()
+        // - 1];
+        return ans;
     }
 };
