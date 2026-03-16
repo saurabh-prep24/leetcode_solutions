d61f55caedcb652e13df829acae7367298083431
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-03-17 01:46:52 +0530

    0034-find-first-and-last-position-of-element-in-sorted-array 03-17-2026

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,9 +4,13 @@ public:
         vector<int> pos(2, -1);
 
         int st = 0, end = nums.size() - 1;
+        // For leftmost pos
+        // store index whenever moving window to left part
+        // and elem is same as target
         while (st <= end) {
             int m = st + (end - st) / 2;
             if (nums[m] >= target) {
+                // update pos when matching target
                 pos[0] = nums[m] == target ? m : -1;
                 end = m - 1;
             } else {
@@ -15,9 +19,13 @@ public:
         }
         st = 0;
         end = nums.size() - 1;
+
+        // for rightmost pos
+        // store pos whenever move to right side part
         while (st <= end) {
             int m = st + (end - st) / 2;
             if (nums[m] <= target) {
+                // update pos when matching target
                 pos[1] = nums[m] == target ? m : -1;
                 st = m + 1;
             } else {
