ca394b26d7eea8dd94bf1049753a6e48d60f8a16
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-04-12 01:25:56 +0530

    3741-minimum-distance-between-three-equal-elements-ii 04-12-2026

--- LeetCode/Medium/3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ LeetCode/Medium/3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -6,20 +6,19 @@ public:
             elems[nums[i]].push_back(i);
         }
         int ans = INT_MAX;
+        int k;
         for (auto i : elems) {
             vector<int> pos = i.second;
             if (pos.size() >= 3) {
-                int temp = INT_MAX;
-                for (int k = 2; k < pos.size(); k++) {
+                for (k = 2; k < pos.size(); k++) {
                     // temp = min(temp, abs(pos[k - 2] - pos[k - 1]) +
                     //  abs(pos[k - 1] - pos[k]) +
                     //  abs(pos[k] - pos[k - 2]));
                     // i,j,k are increasing so
                     // k-j + j-i + k-i is abs
                     // = 2(k-i)
-                    temp = min(temp, 2 * abs(pos[k] - pos[k - 2]));
+                    ans = min(ans, 2 * (pos[k] - pos[k - 2]));
                 }
-                ans = min(ans, temp);
             }
         }
         return ans == INT_MAX ? -1 : ans;
