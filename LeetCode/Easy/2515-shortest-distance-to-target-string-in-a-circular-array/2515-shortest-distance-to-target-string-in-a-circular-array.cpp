a4c09ac56ebfd8b91e9f1c7dee11ceebcdad1dcd
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-04-16 01:36:19 +0530

    2515-shortest-distance-to-target-string-in-a-circular-array 04-16-2026

--- LeetCode/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ LeetCode/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
-        int n = words.size(), start = startIndex;
+        int n = words.size();
         int ans = INT_MAX;
 
-        // right
         for (int i = 0; i < n; i++) {
-            if (words[(i + start) % n] == target) {
-                ans = min(ans, abs(i));
+            // right - (i+startIndex)%n
+            if (words[(i + startIndex) % n] == target) {
+                return i;
+            }
+            // left - (n+startIndex-i)%n
+            if (words[(n + startIndex - i) % n] == target) {
+                return i;
             }
         }
 
-        // left
-        for (int i = 0; i < n; i++) {
-            if (words[(n + start - i) % n] == target) {
-                ans = min(ans, abs(i));
-            }
-        }
-
-        return ans == INT_MAX ? -1 : ans;
+        return -1;
     }
 };
