1c501975d00f456359fb96e2d736308f252f03b1
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-04-20 00:51:06 +0530

    1855-maximum-distance-between-a-pair-of-values 04-20-2026

--- LeetCode/Medium/1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ LeetCode/Medium/1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -50,8 +50,27 @@ public:
         }
         return ans;
     }
+
+    // loop to end with condition
+    // time: O(n)
+    int best(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+        int n1 = nums1.size(), n2 = nums2.size();
+        int i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] <= nums2[j]) {
+                ans = max(ans, j - i);
+                j++;
+            } else {
+                i++;
+                j = i;
+            }
+        }
+        return ans;
+    }
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
         // return brute(nums1, nums2);
-        return opt(nums1, nums2);
+        // return opt(nums1, nums2);
+        return best(nums1, nums2);
     }
 };
