908160d983a09bcf3229a885484527e96978e4af
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-02 02:41:11 +0530

    3742-maximum-path-score-in-a-grid 05-02-2026

--- LeetCode/Medium/3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ LeetCode/Medium/3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,8 +1,9 @@
+
 class Solution {
 public:
     int cost(int x) { return x > 0; }
 
-    void recSolve(vector<vector<int>>& grid, int& k, int n, int m, int x, int y,
+    void recBrute(vector<vector<int>>& grid, int& k, int n, int m, int x, int y,
                   int currScore, int currCost, int& maxScore) {
 
         currScore += grid[x][y];
@@ -19,18 +20,37 @@ public:
 
         // right
         if (y + 1 < m) {
-            recSolve(grid, k, n, m, x, y + 1, currScore, currCost, maxScore);
+            recBrute(grid, k, n, m, x, y + 1, currScore, currCost, maxScore);
         }
 
         // down
         if (x + 1 < n) {
-            recSolve(grid, k, n, m, x + 1, y, currScore, currCost, maxScore);
+            recBrute(grid, k, n, m, x + 1, y, currScore, currCost, maxScore);
         }
     }
 
-    int recSolveMemo(vector<vector<int>>& grid, int k,
-                     vector<vector<vector<int>>>& dp, int x, int y, int n,
-                     int m) {
+    int recSolve(vector<vector<int>>& grid, int k, int n, int m, int x, int y) {
+        if (x >= n || y >= m) {
+            return -1;
+        }
+        if (cost(grid[x][y]) > k) {
+            return -1;
+        }
+        if (x == n - 1 && y == m - 1) {
+            return grid[x][y];
+        }
+
+        int right = recSolve(grid, k - cost(grid[x][y]), n, m, x, y + 1);
+        int down = recSolve(grid, k - cost(grid[x][y]), n, m, x + 1, y);
+        int maxi = max(right, down);
+        if (maxi == -1) {
+            return -1;
+        }
+        return grid[x][y] + maxi;
+    }
+
+    int recSolveMemo(vector<vector<int>>& grid, int k, int x, int y, int n,
+                     int m, vector<vector<vector<int>>>& dp) {
         // invalid
         if (x >= n || y >= m) {
             return -1;
@@ -42,7 +62,7 @@ public:
 
         if (x == n - 1 && y == m - 1) {
             // score at last cell
-            return dp[x][y][k]=grid[x][y];
+            return dp[x][y][k] = grid[x][y];
         }
 
         // already calc so return
@@ -51,30 +71,76 @@ public:
         }
 
         // move right
-        int right = recSolveMemo(grid, k - c, dp, x, y + 1, n, m);
+        int right = recSolveMemo(grid, k - c, x, y + 1, n, m, dp);
         // move down
-        int down = recSolveMemo(grid, k - c, dp, x + 1, y, n, m);
+        int down = recSolveMemo(grid, k - c, x + 1, y, n, m, dp);
         // get max score
         int maxi = max(right, down);
         // if -1 then return
         if (maxi == -1) {
-           return dp[x][y][k] = -1;
+            return dp[x][y][k] = -1;
         }
         // add curr score in maxi
         return dp[x][y][k] = grid[x][y] + maxi;
     }
 
+    int recSolveTab(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(m, vector<int>(k + 1, -1)));
+        for (int x = n - 1; x >= 0; x--) {
+            for (int y = m - 1; y >= 0; y--) {
+                for (int c = k; c >=0; c--) {
+                    int newCost = c + cost(grid[x][y]);
+                    if (newCost > k) {
+                        continue;
+                    }
+
+                    if (x == n - 1 && y == m - 1) {
+                        // score at last cell
+                        dp[x][y][c] = grid[x][y];
+                        continue;
+                    }
+
+                    int right = -1, down = -1;
+                    if (y + 1 < m) {
+                        // move right
+                        right = dp[x][y + 1][newCost];
+                    }
+                    if (x + 1 < n) {
+                        // move down
+                        down = dp[x + 1][y][newCost];
+                    }
+                    // get max score
+                    int maxi = max(right, down);
+                    // if -1 then continue else update
+                    if (maxi != -1) {
+                        // add curr score in maxi
+                        dp[x][y][c] = grid[x][y] + maxi;
+                    }
+                }
+            }
+        }
+        return dp[0][0][0];
+    }
+
     int maxPathScore(vector<vector<int>>& grid, int k) {
         // int ans = -1;
         // int n = grid.size(), m = grid[0].size();
-
-        // recSolve(grid, k, n, m, 0, 0, 0, 0, ans);
+        // recBrute(grid, k, n, m, 0, 0, 0, 0, ans);
         // return ans;
 
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(k + 1, INT_MIN)));
-        int ans = recSolveMemo(grid, k, dp, 0, 0, n, m);
-        return ans != INT_MIN ? ans : -1;
+        // int ans = -1;
+        // int n = grid.size(), m = grid[0].size();
+        // ans = recSolve(grid, k, n, m, 0, 0);
+        // return ans;
+
+        // int n = grid.size(), m = grid[0].size();
+        // vector<vector<vector<int>>> dp(
+        //     n, vector<vector<int>>(m, vector<int>(k + 1, INT_MIN)));
+        // int ans = recSolveMemo(grid, k, 0, 0, n, m, dp);
+        // return ans != INT_MIN ? ans : -1;
+
+        return recSolveTab(grid, k);
     }
 };
