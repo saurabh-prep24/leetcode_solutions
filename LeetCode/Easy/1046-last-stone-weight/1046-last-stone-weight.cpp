7a59ecfe77198c75e60d6bd6f284c8b22defdd2c
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-04 01:51:02 +0530

    1046-last-stone-weight 05-04-2026

---
+++ LeetCode/Easy/1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for (auto i : stones) {
+            pq.push(i);
+        }
+
+        while (pq.size() > 1) {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            // push rem again
+            if (x != y) {
+                pq.push(x - y);
+            }
+        }
+
+        return pq.empty() ? 0 : pq.top();
+    }
+};
