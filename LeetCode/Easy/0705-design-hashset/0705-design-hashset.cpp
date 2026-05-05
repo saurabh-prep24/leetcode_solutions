4d5e73e5fcdd9e28a34674de520771a4caf1a9ce
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-06 02:04:37 +0530

    0705-design-hashset 05-06-2026

---
+++ LeetCode/Easy/0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,20 @@
+class MyHashSet {
+public:
+    int s = 1e6+1;
+    vector<int> v;
+    MyHashSet() { v = vector<int>(s, -1); }
+
+    void add(int key) { v[key] = 1; }
+
+    void remove(int key) { v[key] = -1; }
+
+    bool contains(int key) { return v[key] == 1; }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
