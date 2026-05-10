eaed66826c7ea6da13775824ee710e0a859fb274
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-10 15:06:44 +0530

    2770-maximum-number-of-jumps-to-reach-the-last-index 05-10-2026

--- LeetCode/Medium/2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ LeetCode/Medium/2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int rec(vector<int>& nums, int target, int i) {
         int n = nums.size();
+        // final dest
         if (i == n - 1) {
             return 0;
         }
+        // start ans = -1
         int ans = -1;
         for (int j = i + 1; j < n; j++) {
+            // check all valid j and call for rec
             if (abs(nums[i] - nums[j]) <= target) {
                 int t = rec(nums, target, j);
+                // if valid then update ans
                 if (t != -1)
+                    // consider current jump so 1+t
                     ans = max(ans, 1 + t);
             }
         }
@@ -34,14 +39,39 @@ public:
         }
         return dp[i] = ans;
     }
+
+    int tab(vector<int>& nums, int target) {
+        int n = nums.size();
+        // dp tab
+        vector<int> dp(n + 1, -1);
+        // last index jump = 0
+        dp[n - 1] = 0;
+        // explore reverse of recMemo
+        for (int i = n - 2; i >= 0; i--) {
+            int ans = -1;
+            for (int j = i + 1; j < n; j++) {
+                if (abs(nums[i] - nums[j]) <= target) {
+                    // check already solved dp[j]
+                    int t = dp[j];
+                    if (t != -1)
+                        ans = max(ans, 1 + t);
+                }
+            }
+            // store curr ans
+            dp[i] = ans;
+        }
+        // final ans
+        return dp[0];
+    }
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
         // int ans = rec(nums, target, 0);
         // return ans > 0 ? ans : -1;
 
-        vector<int> dp(n + 1, INT_MIN);
-        int ans = recMemo(nums, target, 0, dp);
-        return ans != INT_MIN ? ans : -1;
+        // vector<int> dp(n + 1, INT_MIN);
+        // return recMemo(nums, target, 0, dp);
+
+        return tab(nums, target);
 
         // choice is there at each index to either take or not
         // ex- [1 0 2]
