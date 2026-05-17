4aebcc889c6c43152c789fd7ae76b5b767f92042
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-17 15:11:14 +0530

    1306-jump-game-iii 05-17-2026

--- LeetCode/Medium/1306-jump-game-iii/1306-jump-game-iii.cpp
+++ LeetCode/Medium/1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    bool rec(vector<int>& arr, int start, vector<bool>& vis) {
+    // Time:  O(n)
+    // Space: O(n)
+    // instead of vis we can also mark arr[start] as (-)
+    // as arr[i]>0 given in ques so would work
+    // so space can be avoided
+    bool dfs(vector<int>& arr, int start, vector<bool>& vis) {
         if (start < 0 || start >= arr.size()) {
             return 0;
         }
@@ -17,12 +22,47 @@ public:
             return 1;
         }
         // return ans
-        return rec(arr, start + arr[start], vis) ||
-               rec(arr, start - arr[start], vis);
+        return dfs(arr, start + arr[start], vis) ||
+               dfs(arr, start - arr[start], vis);
+    }
+
+    // Time:  O(n)
+    // Space: O(n)
+    bool bfs(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<bool> vis(n, false);
+        queue<int> q;
+        q.push(start);
+        vis[start] = true;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            if (arr[node] == 0) {
+                return true;
+            }
+            int add = node + arr[node];
+            int sub = node - arr[node];
+            // add jump
+            if (add >= 0 && add < n && !vis[add]) {
+                vis[add] = true;
+                q.push(add);
+            }
+            // subtract jump
+            if (sub >= 0 && sub < n && !vis[sub]) {
+                vis[sub] = true;
+                q.push(sub);
+            }
+        }
+        return false;
     }
     bool canReach(vector<int>& arr, int start) {
+        if(arr[start]==0)
+            return 1;
+
         // track vis
-        vector<bool> vis(arr.size(), 0);
-        return rec(arr, start, vis);
+        // vector<bool> vis(arr.size(), 0);
+        // return dfs(arr, start, vis);
+
+        return bfs(arr, start);
     }
 };
