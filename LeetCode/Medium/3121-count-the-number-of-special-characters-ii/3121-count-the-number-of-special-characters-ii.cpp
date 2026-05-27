65948c4fea6c57b4d994cca12eb9f8c94c5ae3ee
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-28 00:03:03 +0530

    3121-count-the-number-of-special-characters-ii 05-28-2026

--- LeetCode/Medium/3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ LeetCode/Medium/3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numberOfSpecialChars(string word) {
+    int solve1(string word) {
         // store last index for each lowercase
         vector<int> lower(26, -1);
         for (int i = 0; i < word.size(); i++) {
@@ -26,5 +26,31 @@ public:
             }
         }
         return ans;
+    }
+    int solve2(string word) {
+        // store last index for each lowercase
+        vector<int> lower(26, -1);
+        // store first index for each uppercase
+        vector<int> upper(26, -1);
+        for (int i = 0; i < word.size(); i++) {
+            char c = word[i];
+            if (c >= 'a' && c <= 'z') {
+                // overwrite to keep last index
+                lower[c - 'a'] = i;
+            } else if (upper[c - 'A'] == -1) {
+                upper[c - 'A'] = i;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+    int numberOfSpecialChars(string word) {
+        // return solve1(word);
+        return solve2(word);
     }
 };
