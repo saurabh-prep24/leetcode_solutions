b6a4eaf393baed73e5f5c9242b9782f3a81e0df5
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-27 23:58:50 +0530

    3121-count-the-number-of-special-characters-ii 05-27-2026

--- LeetCode/Medium/3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ LeetCode/Medium/3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
+        // store last index for each lowercase
         vector<int> lower(26, -1);
         for (int i = 0; i < word.size(); i++) {
             char c = word[i];
             if (c >= 'a' && c <= 'z') {
+                // overwrite to keep last index
                 lower[c - 'a'] = i;
             }
         }
         int ans = 0;
         for (int i = 0; i < word.size(); i++) {
             char c = word[i];
+            // check only now for uppercase
             if (c >= 'A' && c <= 'Z') {
-                cout << i << c << endl;
+                // if lowercase last index != -1 and < i
+                // then increment ans
                 if (lower[c - 'A'] != -1 && lower[c - 'A'] < i) {
                     ans++;
                 }
+                // count this char as invalid now
+                // as only first occurrence of upper needs consideration
                 lower[c - 'A'] = -1;
             }
         }
