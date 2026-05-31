9f40ed7b93de1e86dac1afdb97d2409a8b31abad
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-05-31 20:23:29 +0530

    2126-destroying-asteroids 05-31-2026

--- LeetCode/Medium/2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ LeetCode/Medium/2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+    // time: O(n logn)
+    // time: O(1)
+    bool sortingSolve(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(), asteroids.end());
         int i = 0;
         long long currMass = mass;
@@ -15,5 +17,38 @@ public:
             i++;
         }
         return 1;
+    }
+    // time: O(n logn)
+    // time: O(n)
+    bool heapSolve(int mass, vector<int>& asteroids) {
+        int i = 0;
+        long long currMass = mass;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        while (i < asteroids.size()) {
+            if (currMass < asteroids[i]) {
+                // not possible to destroy
+                pq.push(asteroids[i]);
+            } else {
+                // merged to mass
+                currMass += asteroids[i];
+            }
+            i++;
+        }
+        if (pq.size() == asteroids.size()) {
+            return 0;
+        }
+        while (!pq.empty()) {
+            cout << pq.top() << endl;
+            if (pq.top() > currMass) {
+                return 0;
+            }
+            currMass += pq.top();
+            pq.pop();
+        }
+        return pq.empty();
+    }
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        // return sortingSolve(mass, asteroids);
+        return heapSolve(mass, asteroids);
     }
 };
