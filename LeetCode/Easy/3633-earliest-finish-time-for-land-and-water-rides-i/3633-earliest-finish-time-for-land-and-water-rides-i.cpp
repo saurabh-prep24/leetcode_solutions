ac74251b2aeba357b913c24322ea8feeffbf40bd
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-06-03 02:36:24 +0530

    3633-earliest-finish-time-for-land-and-water-rides-i 06-03-2026

--- LeetCode/Easy/3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ LeetCode/Easy/3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -9,20 +9,22 @@ public:
             for (int j = 0; j < waterStartTime.size(); j++) {
                 // Land->Water
                 int d = landStartTime[i] + landDuration[i];
-                if (waterStartTime[j] <= d) {
-                    d += waterDuration[j];
-                } else {
-                    d = waterStartTime[j] + waterDuration[j];
-                }
+                // if (waterStartTime[j] <= d) {
+                //     d += waterDuration[j];
+                // } else {
+                //     d = waterStartTime[j] + waterDuration[j];
+                // }
+                d = max(d, waterStartTime[j]) + waterDuration[j];
                 ans = min(ans, d);
 
                 // Water->Land
                 int d2 = waterStartTime[j] + waterDuration[j];
-                if (landStartTime[i] <= d2) {
-                    d2 += landDuration[i];
-                } else {
-                    d2 = landStartTime[i] + landDuration[i];
-                }
+                // if (landStartTime[i] <= d2) {
+                //     d2 += landDuration[i];
+                // } else {
+                //     d2 = landStartTime[i] + landDuration[i];
+                // }
+                d2 = max(d2, landStartTime[i]) + landDuration[i];
                 ans = min(ans, d2);
             }
         }
