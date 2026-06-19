756848a45cba784a8f28603a83dd4f5ed15e520e
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-06-20 03:10:35 +0530

    1732-find-the-highest-altitude 06-20-2026

---
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans = 0, sum = 0;
+        for (auto i : gain) {
+            sum += i;
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
