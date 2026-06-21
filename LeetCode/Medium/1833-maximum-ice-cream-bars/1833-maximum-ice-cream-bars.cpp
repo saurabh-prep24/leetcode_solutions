5ece3c78869d0e2b98e780de75151c1ce4181d05
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-06-21 12:07:02 +0530

    1833-maximum-ice-cream-bars 06-21-2026

---
+++ LeetCode/Medium/1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int ans = 0;
+        sort(costs.begin(), costs.end());
+        for (int i = 0; i < costs.size(); i++) {
+            if (coins - costs[i] >= 0) {
+                ans += 1;
+                coins -= costs[i];
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+};
