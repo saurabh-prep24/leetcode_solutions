d74aaa43d9b9485d540e9406cedd484982454cd9
saurabh-prep24 <saurabh.prep24@gmail.com>
2026-07-07 03:01:49 +0530

    1288-remove-covered-intervals 07-07-2026

--- LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     // time: O(n*logn)
     // space: O(n)
-    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+    int solve1(vector<vector<int>>& intervals) {
         // sort by start time
         sort(intervals.begin(), intervals.end());
         int n = intervals.size();
-        
+
         // track prev intervals
         stack<pair<int, int>> prevInterval;
         // push intial duration
         prevInterval.push({intervals[0][0], intervals[0][1]});
-        
+
         // check other intervals
         for (int i = 1; i < n; i++) {
             // curr interval
@@ -36,5 +36,40 @@ public:
         }
         // return size as count of intervals
         return prevInterval.size();
+    }
+
+    struct Comp {
+        bool operator()(vector<int>& a, vector<int>& b) {
+            if (a[0] == b[0]) {
+                return b[1] < a[1];
+            }
+            return a[0] < b[0];
+        }
+    };
+    // time: O(n*logn)
+    // space: O(1)
+    int solveOpt(vector<vector<int>>& intervals) {
+        // sort by start time and longer end time for same start time
+        sort(intervals.begin(), intervals.end(), Comp());
+        int n = intervals.size();
+
+        // track end of interval
+        int last = 0;
+        int ans = 0;
+        // check intervals
+        for (int i = 0; i < n; i++) {
+            // curr interval end
+            int e = intervals[i][1];
+
+            if (e > last) {
+                ans++;
+                last = max(last, e);
+            }
+        }
+        return ans;
+    }
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        // return solve1(intervals);
+        return solveOpt(intervals);
     }
 };
